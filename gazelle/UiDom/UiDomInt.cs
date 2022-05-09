4186cc54e30aecc2af90d9459c8a1aae134ee304
Esme Povirk <madewokherd@gmail.com>
2022-05-09 11:44:23 -0500

    Add integer literal support.

---
+++ gazelle/UiDom/UiDomInt.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gazelle.UiDom
+{
+    internal class UiDomInt : UiDomValue
+    {
+        public UiDomInt(int value)
+        {
+            Value = value;
+        }
+
+        public int Value { get; }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(this, obj))
+                return true;
+            if (obj is UiDomInt i)
+                return Value == i.Value;
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return Value ^ typeof(UiDomInt).GetHashCode();
+        }
+
+        public override bool ToBool()
+        {
+            return Value != 0;
+        }
+
+        public override string ToString()
+        {
+            return Value.ToString();
+        }
+    }
+}
