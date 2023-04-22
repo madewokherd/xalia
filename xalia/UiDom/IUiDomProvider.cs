a0077a463813758d3db9b5988318c3d6016fb7d9
Esme Povirk <madewokherd@gmail.com>
2023-04-22 13:22:03 -0500

    Add "providers" for UiDomElements.
    This will eventually obsolete IUiDomApplication and all
    UiDomElement subclasses.

---
+++ xalia/UiDom/IUiDomProvider.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xalia.Gudl;
+
+namespace Xalia.UiDom
+{
+    public interface IUiDomProvider
+    {
+        void NotifyElementRemoved(UiDomElement element);
+
+        // returns True if handled
+        bool WatchProperty(UiDomElement element, GudlExpression expression);
+
+        bool UnwatchProperty(UiDomElement element, GudlExpression expression);
+
+        UiDomValue EvaluateIdentifier(UiDomElement element, string identifier,
+            HashSet<(UiDomElement, GudlExpression)> depends_on);
+
+        UiDomValue EvaluateIdentifierLate(UiDomElement element, string identifier,
+            HashSet<(UiDomElement, GudlExpression)> depends_on);
+
+        void TrackedPropertyChanged(UiDomElement element, string name, UiDomValue new_value);
+
+        void DumpProperties(UiDomElement element);
+
+        Task<(bool, int, int)> GetClickablePointAsync(UiDomElement element);
+    }
+}
