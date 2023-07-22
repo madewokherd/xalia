fa86c0bc80cba33cf2ff04c7fb6fcf4299010e9c
Esme Povirk <madewokherd@gmail.com>
2023-07-22 12:06:54 -0500

    Win32: Add support for "selected" property on tab items.

--- xalia/Win32/HwndTabItemProvider.cs
+++ xalia/Win32/HwndTabItemProvider.cs
@@ -15,6 +15,13 @@ namespace Xalia.Win32
         public HwndTabProvider Parent { get; }
         public int ChildId { get; }
 
+        public override void DumpProperties(UiDomElement element)
+        {
+            if (Parent.SelectionIndexKnown)
+                Utils.DebugWriteLine($"  selected: {Parent.SelectionIndex == ChildId - 1}");
+            base.DumpProperties(element);
+        }
+
         public override UiDomValue EvaluateIdentifier(UiDomElement element, string identifier, HashSet<(UiDomElement, GudlExpression)> depends_on)
         {
             switch (identifier)
@@ -23,6 +30,19 @@ namespace Xalia.Win32
                     return UiDomBoolean.True;
             }
             return base.EvaluateIdentifier(element, identifier, depends_on);
+        }
+
+        public override UiDomValue EvaluateIdentifierLate(UiDomElement element, string identifier, HashSet<(UiDomElement, GudlExpression)> depends_on)
+        {
+            switch (identifier)
+            {
+                case "selected":
+                    depends_on.Add((Parent.Element, new IdentifierExpression("win32_selection_index")));
+                    if (Parent.SelectionIndexKnown)
+                        return UiDomBoolean.FromBool(Parent.SelectionIndex == ChildId - 1);
+                    break;
+            }
+            return base.EvaluateIdentifierLate(element, identifier, depends_on);
         }
     }
 }
